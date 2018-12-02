04989c225adb3b46239583a7aaa8d3b7836daee5
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-01 18:45:58 -0600

    initial build out

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { EntryEditorComponent } from './entry-editor/entry-editor.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'workouts', component: WorkoutsComponent },
+  { path: 'workouts/:id', component: EntryEditorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
