5c994c0a51da190bd35ee6c581443f58f80b3584
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-02 20:51:06 -0600

    progress bars

--- src/app/workouts/workouts.component.ts
+++ src/app/workouts/workouts.component.ts
@@ -16,6 +16,7 @@ export class WorkoutsComponent implements OnInit {
   public workouts = [];
   public loading = false;
   private perfTargets = {};
+  public totals = {};
 
   constructor(private api: WorkoutsApiService, private modal: NgbModal) {}
 
@@ -24,6 +25,7 @@ export class WorkoutsComponent implements OnInit {
       ([workoutsResult, perfTargetsResult]) => {
         this.workouts = workoutsResult;
         this.perfTargets = perfTargetsResult;
+        this.calculatePerformance();
         this.loading = false;
         console.log('**workouts', this.workouts, this.perfTargets);
       }
@@ -55,6 +57,27 @@ export class WorkoutsComponent implements OnInit {
     }, reason => {
       console.log(`Dismissed reason: ${reason}`);
     });
+  }
+
+  calculatePerformance() {
+    const bikeTotal = _.chain(this.workouts).filter(x => x.type === 'bike').sumBy(x => +x.distance).value();
+    const rowTotal = _.chain(this.workouts).filter(x => x.type === 'row').sumBy(x => +x.distance).value();
+    const runTotal = _.chain(this.workouts).filter(x => x.type === 'run').sumBy(x => +x.distance).value();
+    this.totals = { bike: bikeTotal, row: rowTotal, run: runTotal };
+    console.log('**totals', this.totals);
+  }
+
+  getPBType(total: number, target: number) {
+    const pct = (total / target) * 100;
+    if (pct < 25) {
+      return 'success';
+    } else if (pct > 25 && pct < 50) {
+      return 'info';
+    } else if (pct > 50 && pct < 75) {
+      return 'warning';
+    }  else if (pct > 75) {
+      return 'danger';
+    }
   }
 
 }
